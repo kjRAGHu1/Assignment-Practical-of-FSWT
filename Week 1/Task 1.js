61056b4f4ab6e687989ec94e68f23ebddeb4fdd7
Kushal <21it050@charusat.edu.in>
2023-06-28 14:52:11 +0530

    First Commit

---
+++ Week 1/Task 1.js
@@ -0,0 +1,18 @@
+var Var1 = "Hello World!";
+console.log("Value of Var1:", Var1);
+
+document.write( "Value of Var1 is : " + Var1);
+document.write("<br>");
+
+
+let Var2 = 58;
+console.log("Value of Var2:", Var2);
+
+document.write( "Value of Var2 is : " + Var2);
+document.write("<br>");
+
+const Var3 = true;
+console.log("Value of var3:", Var3);
+
+document.write( "Value of Var3 is : " + Var3);
+document.write("<br>");
